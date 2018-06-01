94879e9c88eb12d71f51f51775fe5bf09d897133
csurran2 <jcp15320@gmail.com>
2018-06-01 00:23:05 -0500

    Update ascii.py

--- ascii.py
+++ ascii.py
@@ -1,5 +1,5 @@
 import random
 import os
-i = random.randrange(9) + 1 
+i = random.randrange(0) + 1 
 website="https://raw.githubusercontent.com/csurran2/ASCII-Art-Splash-Screen/master/art/"+ str(i) + ".txt"
 os.system("curl " +  website)
